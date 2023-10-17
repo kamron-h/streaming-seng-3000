22061d593e321adc29e3260d763255925d209c3b
Kamron <kamronthopkins@gmail.com>
2023-10-17 00:52:13 +0000

    Fixed static file path bug in settings

--- demo/settings.py
+++ demo/settings.py
@@ -129,10 +129,10 @@ USE_TZ = True
 
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "public/static"),
+    os.path.join(BASE_DIR, "assets"),
 ]
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
